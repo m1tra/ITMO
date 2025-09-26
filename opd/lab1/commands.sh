f33664c78183dbe006161cd7469e3bd31fed75e5
Vladimir <vkolosov1@mail.ru>
2025-09-26 16:00:48 +0300

    lab1 opd

---
+++ opd/lab1/commands.sh
@@ -0,0 +1,56 @@
+mkdir -p lab0/floatze15/{feebas,grotle,mandibuzz} lab0/onix9/{lombre,phanpy,gurdurr,swablu} lab0/dragonair lab0/cherubi lab0/nidorino
+
+echo "weigth=332.0 height=55.0 atk=8 def=9" > lab0/floatze15/walrein
+echo -e "Развитые\nспособности Hustle" > lab0/floatze15/nidoranM
+echo -e "Возможности Overland=1 Surface=3\nSky=12 Power=2 Intelligence=4 Invisbility=0 Phasing=0" > lab0/mismagius4
+echo -e "satk=3 sdef=3\nspd=3" > lab0/sunkern6
+echo -e "Развитые способabilities Overcoat" > lab0/wormadam7 
+echo -e "Тип\nдиеты Omnivore" > lab0/onix9/skitty
+echo "satk=7 sdef=8 spd=9" > lab0/onix9/golbat
+
+chmod u=wx,g=x,o=w floatze15
+chmod u=rw, g=r,o= floatze15/walrein
+chmod u=rx,g=rwx,o=rwx floatze15/feebas
+chmod 550 floatze15/grotle
+chmod 751 floatze15/mandibuzz
+chmod 660 floatze15/nidoranM
+chmod 764 mienfoo0 
+chmod 624 mienfoo0/dragonair
+chmod 551 mienfoo0/cherubi
+chmod 733 mienfoo0/nidorino
+chmod 624 mismagius4
+chmod 511 onix9
+chmod 060 onix9/skitty
+chmod 620 onix9/golbat
+chmod 355 onix9/lombre
+chmod 755 onix9/phanpy
+chmod 573 onix9/gurdurr
+chmod 751 onix9/swablu
+chmod 400 sunkern6
+chmod 064 wormadam7
+
+chmod u+r lab0/onix9/skitty
+cat lab0/onix9/golbat lab0/onix9/skitty > lab0/mismagius4_62
+cp -r lab0/mienfoo0 lab0/onix9/swablu
+chmod u+w lab0/onix9
+ln -s ../sunkern6 lab0/onix9/golbatsunkern
+chmod u+w lab0/onix9/gurdurr
+cp lab0/sunkern6 lab0/onix9/gurdurr
+cat lab0/mismagius4 > lab0/onix9/skittymismagius
+ln -s mienfoo0 lab0/Copy_23 
+ln lab0/sunkern6 lab0/floatzel5/walreinsunkern
+
+wc -l lab0/floatzel5/nidoranM lab0/onix9/skitty 2>/dev/null > /tmp/count.txt
+ls -lRt lab0 2>&1 | grep 'y$'
+cat -n lab0/sunkern6 2>/tmp/errors.txt | sort -r
+cat lab0/mienfoo0 2>/dev/null | sort -r 
+wc -m lab0/sunkern6 >> lab0/sunkern6 2>&1
+ls -lR lab0 2>&1 | grep '^[\-d]' | sort -k5 -n | head -n 2  
+
+rm lab0/mismagius4
+rm lab0/onix9/golbat
+rm lab0/Copy_*
+rm lab0/floatzel5/walreinsunke* 
+chmod -R u+rw lab0/onix9
+rm -r lab0/onix9
+rmdir lab0/floatzel5/grotle
